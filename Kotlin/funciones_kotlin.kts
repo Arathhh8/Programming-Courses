3f98ae2e7f2758570a900b02aa2d273f7915e982
Arath García <arathgg143@gmail.com>
2022-12-24 17:46:54 -0600

    kotlin folder added

---
+++ Kotlin/funciones_kotlin.kts
@@ -0,0 +1,44 @@
+
+
+
+// CÓDIGO CON 3 FUNCIONES, SUMA DOS NUMEROS, REDONDEA Y MUESTRA VALOR CON FORMATO DE VENTA
+/*var result = suma(1.0, 10.45f)
+var resultRed = redondeo(result)
+muestra_resulatdo(resultRed)
+
+fun muestra_resulatdo(num : Double){
+    println(String.format("$%.2f",num))
+}
+    // FUNCION QUE REDONDEA EL NUMERO HACIA ARRIBA
+redondeo(2.5)
+fun redondeo(num1 : Double) : Double{
+    var redondeado : Int = num1.toInt()
+    if(num1 > redondeado){
+        redondeado++
+    }
+    return redondeado.toDouble()
+}
+
+    //FUNCION QUE RECIBE DOS NUMEROS Y LOS SUMA
+fun suma(num1 : Double ,num2f : Float) : Double {
+
+    var result : Double = num1 + num2f
+    return result
+}*/
+
+/*printHello()
+recibeArg("Arath2")
+var value = returnValue(22)
+println(value)
+fun printHello(){
+    println("hello")
+}
+
+fun recibeArg(name: String){
+    println(name)
+}
+
+fun returnValue(num : Int) : Int {
+    var result : Int = num + 2
+    return result
+}*/
